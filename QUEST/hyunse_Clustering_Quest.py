315c365e2240d46a6b7d126632f570c2d4cdb2c0
myeonsei <38792588+myeonsei@users.noreply.github.com>
2018-06-30 14:43:16 +0900

    Update hyunse_Clustering_Quest.py

--- QUEST/hyunse_Clustering_Quest.py
+++ QUEST/hyunse_Clustering_Quest.py
@@ -1,19 +1,23 @@
 from sklearn.cluster import KMeans
+from matplotlib import pyplot as plt
+from scipy.misc import imread, imsave
 
-def recolor_image(input_file, k=5):
-
-    img = mpimg.imread(path_to_png_file)
-    pixels = [pixel for row in img for pixel in row]
+#def recolor_image(input_file, k=5):
     
-    model = KMeans(n_clusters = k, algorithm = 'auto')
-    model.fit(pixels)
-    
-    def recolor(pixel):
-         return model.predict(pixel)
+input_file = 'C:\\Users\\myeon\\Desktop\\Data Science\\0602 클러스터링\\sample.png'; k=5
+#recolor_image(path)
 
-    new_img = [[recolor(pixel) for pixel in row]
-               for row in img]
+img = imread(input_file)
+pixels = [pixel for row in img for pixel in row]
 
-    plt.imshow(new_img)
-    plt.axis('off')
-    plt.show()
+model = KMeans(n_clusters = k, algorithm = 'auto')
+model.fit(pixels)
+
+def recolor(pixel):
+    return model.predict(pixel)
+
+new_img = [recolor(row) for row in img]
+
+plt.imshow(new_img)
+plt.axis('off')
+plt.show()
